b10ea29320b6f520741ac36eea096cb3abc0979f
Justus Braun <justusbraun03@gmail.com>
2021-05-19 23:21:35 +0200

    Add Camera Button
    When pressed an Alert will pop out

--- certificate-app-frontend/App.js
+++ certificate-app-frontend/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <View style={styles.button}>
+        <Button
+          title='Camera'
+          color="#000"
+          onPress={() => Alert.alert('Camera geöffnet')}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808080',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#000',
+    borderRadius: 10,
+    borderWidth: 1,
+    width: 300,
+    borderColor: '#fff'
   },
 });
