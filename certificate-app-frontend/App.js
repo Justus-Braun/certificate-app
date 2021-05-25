90a694636edb1a6adfa06b2ec85d0992e8beb183
Justus Braun <57273189+Justus-Braun@users.noreply.github.com>
2021-05-25 18:48:51 +0200

    Revert "Added Photo Button"

--- certificate-app-frontend/App.js
+++ certificate-app-frontend/App.js
@@ -1,70 +1,21 @@
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { OpenCamera } from './classes/Camera.js';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.otherContainer}>
-        <Button
-        title="Camera"
-        onPress={() => navigation.navigate('Camera')
-        }>
-        </Button>
-      </View>
-    </View>
-  )
-}
-
-const Stack = createStackNavigator();
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'certificate-app', headerTitleStyle: { alignSelf: 'center' }}}/>
-        <Stack.Screen name="Camera" component={OpenCamera} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-var styles = StyleSheet.create({
-
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  topBox: {
-    flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headline: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 24,
-    marginTop: 0,
-    width: 200,
-  },
-  marginTop: {
-    width: 200,
-    height: 80,
-  },
-  otherContainer: {
-    flex: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green',
-  },
-  button: {
-
   },
 });
