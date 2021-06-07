fdfc99244a1128761d0d83e7a0aec0c6fced0ab4
Kieberts <65255153+Kieberts@users.noreply.github.com>
2021-06-07 12:12:59 +0200

    Update NewCamera.js

--- certificate-app-frontend/classes/NewCamera.js
+++ certificate-app-frontend/classes/NewCamera.js
@@ -1,9 +1,14 @@
 import {Camera} from 'expo-camera';
 
-
+const styles = StyleSheet.create({
+  cameraStyle:{
+  
+  },
+});
 
 export function OpenCamera2() {
     return {
-        <Camera style={}
+        <Camera style={styles.cameraStyle}>
+        </Camera>
     }
 }
