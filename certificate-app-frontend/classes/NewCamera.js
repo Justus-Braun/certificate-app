566d30f89ef3c2d940243094067bb8d2f9a69c4b
Kieberts <65255153+Kieberts@users.noreply.github.com>
2021-06-07 12:16:17 +0200

    Update NewCamera.js

--- certificate-app-frontend/classes/NewCamera.js
+++ certificate-app-frontend/classes/NewCamera.js
@@ -1,14 +1,30 @@
 import {Camera} from 'expo-camera';
 
 const styles = StyleSheet.create({
-  cameraStyle:{
-  
+  cameraStyle: {
+    flex: 1,
+    width: "100%"
   },
 });
 
 export function OpenCamera2() {
     return {
-        <Camera style={styles.cameraStyle}>
+        <Camera 
+          style={styles.cameraStyle}
+          ref={(r) => {
+            camera = r
+          }}
+        >      
         </Camera>
     }
+}
+
+const __startCamera = async () => {
+  const {status} = await Camera.requestPermissionAsync()
+  
+  if(status === 'granted') {
+    // do something 
+  }else{
+    Alert.alert("Acces")
+  }
 }
