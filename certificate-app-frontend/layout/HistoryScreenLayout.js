735bead84a72df726a1a4cb62c3fe200c66c993b
Kieberts <65255153+Kieberts@users.noreply.github.com>
2021-06-14 21:29:56 +0200

    Add History File

---
+++ certificate-app-frontend/layout/HistoryScreenLayout.js
@@ -0,0 +1,12 @@
+import {Text, TouchableOpacity, View} from "react-native";
+import {stylesCamera} from "../styles/homeScreen";
+import React from "react";
+
+export function OpenHistory() {
+
+    return (
+        <View style={stylesCamera.container}>
+            <Text>This is the History Page!</Text>
+        </View>
+    );
+};
